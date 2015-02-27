561b2d41a81c4ca192f16870eff5a35c70d2e7c2
Noa Resare <noa@spotify.com>
2015-02-27 17:47:35 +0100

    implemented variable sleeps in SustainableRateClient

--- src/main/java/com/spotify/rosehill/RosehillMain.java
+++ src/main/java/com/spotify/rosehill/RosehillMain.java
@@ -1,19 +1,13 @@
 package com.spotify.rosehill;
 
-import com.google.common.util.concurrent.FutureCallback;
-import com.google.common.util.concurrent.Futures;
 import com.spotify.folsom.ConnectFuture;
 import com.spotify.folsom.MemcacheClient;
 import com.spotify.folsom.MemcacheClientBuilder;
-import com.spotify.folsom.MemcacheStatus;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.Map;
-import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
-import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * The main class
@@ -28,46 +22,16 @@ public class RosehillMain {
     memcacheClient = getClient(memcacheHost);
   }
 
-  public static void main(String[] args) {
+  public static void main(String[] args) throws Throwable {
 
     RosehillMain main = new RosehillMain("trusty0");
     main.populate(1000 * 1000);
   }
 
-  private void populate(int count) {
-    final CountDownLatch latch = new CountDownLatch(count);
-    final AtomicInteger outstanding = new AtomicInteger();
-    for (int i = 0; i < count; i++) {
-      if (i % 1000 == 0) {
-        log.info("issuing write " + i);
-      }
-      outstanding.incrementAndGet();
-      Futures.addCallback(memcacheClient.replace(Integer.toString(i), "foobar", 0),
-          new FutureCallback<MemcacheStatus>() {
-            @Override
-            public void onSuccess(MemcacheStatus result) {
-              if (result != MemcacheStatus.OK) {
-                throw new RuntimeException("Store failed, server returned " + result);
-              }
-              log.debug("Got another response currently outstanding: " + outstanding.decrementAndGet());
-              latch.countDown();
-            }
 
-
-            @Override
-            public void onFailure(Throwable t) {
-              latch.countDown();
-              throw new RuntimeException("Store failed", t);
-            }
-          });
-    }
-    try {
-      latch.await();
-      log.info("latch is now counted down");
-
-    } catch (InterruptedException e) {
-      throw new RuntimeException(e);
-    }
+  private void populate(int count) throws Throwable {
+    SustainableRateClient rateClient = new SustainableRateClient(memcacheClient, 900);
+    rateClient.runSetCommands(count);
     memcacheClient.shutdown();
   }
 
--- src/test/java/com/spotify/rosehill/MainRunner.java
+++ src/test/java/com/spotify/rosehill/MainRunner.java
@@ -4,7 +4,7 @@ package com.spotify.rosehill;
  * Created by noa on 27/02/15.
  */
 public class MainRunner {
-  public static void main(String[] args) {
+  public static void main(String[] args) throws Throwable{
     RosehillMain.main(args);
   }
 }
